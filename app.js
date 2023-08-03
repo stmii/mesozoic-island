46322342da70719a50ba303a66e2c9bead084956
Steven McIntyre <mcintyst@flip2.engr.oregonstate.edu>
2023-08-03 12:41:45 -0700

    change PORT to 1712

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 1218;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 1712;                 // Set a port number at the top so it's easy to change in the future
 
 /*
 	ROUTES
