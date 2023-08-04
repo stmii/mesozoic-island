ff0223e4c065cb63a9a315b9a3b79b06bc1ebd8d
Anisa <111161857+anisar17@users.noreply.github.com>
2023-08-03 23:21:16 -0700

    Add files via upload

--- public/js/update_species.js
+++ public/js/update_species.js
@@ -14,5 +14,55 @@ updateSpecies.addEventListener("submit", function (e) {
     let inputDiet = document.getElementById('input_diet_update');
 
     // Get values from the form fields
-    
+    let speciesNameValue = inputSpeciesName.value;
+    let periodValue = inputPeriod.value;
+    let typeValue = inputType.value;
+    let dietValue = inputDiet.value;
+
+    // Currently Species table does not allow for updating to NULL so we must exit 
+    if (isNaN(speciesNameValue)) {
+        return;
+    }
+
+    // Put our data we want to send in a JS object
+    let data = {
+        speciesName: speciesNameValue,
+        period: periodValue,
+        type: typeValue,
+        diet: dietValue
+    }
+
+    // Setup AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('PUT', '/put-species-ajax', true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+
+    // Tell AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add updated data to the table 
+            updateRow(xhttp.response, speciesNameValue);
+        }
+        else if (xhttp.readyState == 4 & xhttp.status != 200) {
+            console.log('There was an error with the input')
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
 })
+
+function updateRow(data, speciesID) {
+    let parsedData = JSON.parse(data);
+    let table = document.getElementById('species-table');
+
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        // iterate through rows, rows are accessed using the 'row' variable assigned in the for loop
+        if (table.rows[i].getAttribute('data-value') == speciesID) {
+            let updateRowIndex = table.getElementsByTagName('tr')[i];
+            let td = updateRowIndex.getElementsByTagName('td')[3];
+            td.innerHTML = parsedData[0].name;
+        }
+    }
+}
