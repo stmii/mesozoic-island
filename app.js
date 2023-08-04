37caf4c372c70cd2f32ee20d79876c38ea23c115
Steven McIntyre <mcintyst@flip2.engr.oregonstate.edu>
2023-08-03 21:28:12 -0700

    add SELECT functions

--- app.js
+++ app.js
@@ -5,7 +5,16 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
 var db = require('./database/db-connector.js');
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');
+app.engine('.hbs', engine({extname: ".hbs"}));
+app.set('view engine', '.hbs');
+
 PORT        = 1712;                 // Set a port number at the top so it's easy to change in the future
 
 /*
@@ -13,19 +22,12 @@ PORT        = 1712;                 // Set a port number at the top so it's easy
 */
 app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
 	{
-		// Define our queries
-		query1 = 'INSERT INTO Species (species_name, period, type, diet) VALUES ("Brontosaurus", "Cretaceous", "land", "herbivore");';
-		query2 = 'SELECT * FROM Species;';
+		let query1 = "SELECT * FROM Species;";
 
-		// INSERT
-		db.pool.query(query1, function (err, results, fields){
-			
-			// SELECT *
-			db.pool.query(query2, function (err, results, fields){
-	
-				res.send(JSON.stringify(results));
-			});
-		});
+		db.pool.query(query1, function(error, rows, fields){
+
+			res.render('species', {data: rows});
+		})
 	});
 
 
